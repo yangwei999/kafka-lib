aca357dbb0f0136e92b2becd068aec09e0d0c045
yangwei999 <348134071@qq.com>
2023-04-24 16:33:51 +0800

    1

--- consumer.go
+++ consumer.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/sirupsen/logrus"
 
@@ -10,6 +13,9 @@ import (
 )
 
 func main() {
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
 	if err := kafka.InitV2(mq.Addresses("10.0.0.161:9092")); err != nil {
 		logrus.Fatal(err)
 	}
@@ -29,5 +35,10 @@ func main() {
 		logrus.Fatal(err)
 	}
 
-	defer s.Unsubscribe()
+	select {
+	case sig := <-sigchan:
+		fmt.Printf("Caught signal %v: terminating\n", sig)
+		s.Unsubscribe()
+	}
+
 }
